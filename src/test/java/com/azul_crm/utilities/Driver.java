ae5f5111370f1bffe2285a442869a003ed7ad38e
MariiaBorovykova <mashabobrakova@gmail.com>
2024-03-21 10:50:35 -0400

    update Driver, add headless browser

--- src/test/java/com/azul_crm/utilities/Driver.java
+++ src/test/java/com/azul_crm/utilities/Driver.java
@@ -4,6 +4,7 @@ import com.azul_crm.utilities.ConfigurationReader;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -11,7 +12,8 @@ import java.time.Duration;
 public class Driver {
 
     //create a private constructor to remove access to this object
-    private Driver(){}
+    private Driver() {
+    }
 
     /*
     We make the WebDriver private, because we want to close access from outside the class.
@@ -25,9 +27,9 @@ public class Driver {
     Create a re-usable utility method which will return the same driver instance once we call it.
     - If an instance doesn't exist, it will create first, and then it will always return same instance.
      */
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if(driverPool.get() == null){
+        if (driverPool.get() == null) {
 
             /*
             We will read our browserType from configuration.properties file.
@@ -39,19 +41,28 @@ public class Driver {
             Depending on the browserType returned from the configuration.properties
             switch statement will determine the "case", and open the matching browser.
              */
-            switch (browserType){
+            switch (browserType) {
                 case "chrome":
                     //WebDriverManager.chromedriver().setup();
                     driverPool.set(new ChromeDriver());
                     driverPool.get().manage().window().maximize();
-                    driverPool.get(). manage(). window(). setSize(new Dimension(1920, 1080));
+                    driverPool.get().manage().window().setSize(new Dimension(1920, 1080));
                     driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
                 case "firefox":
                     //WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
                     driverPool.get().manage().window().maximize();
-                    driverPool.get(). manage(). window(). setSize(new Dimension(1920, 1080));
+                    driverPool.get().manage().window().setSize(new Dimension(1920, 1080));
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                case "headless-chrome":
+                    //WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless=new");
+                    driverPool.set(new ChromeDriver(options));
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().window().setSize(new Dimension(1920, 1080));
                     driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
             }
@@ -65,8 +76,8 @@ public class Driver {
     /*
     Create a new Driver.closeDriver(); it will use .quit() method to quit browsers, and then set the driver value back to null.
      */
-    public static void closeDriver(){
-        if (driverPool.get()!=null){
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
             /*
             This line will terminate the currently existing driver completely. It will not exist going forward.
              */
